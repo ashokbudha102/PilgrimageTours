0bfd244750fdd85906c22dcb9a30db07dd801cd4
bishalkhanal <bishal.khanal.2057@gmail.com>
2021-02-13 16:54:55 +0545

    added about and login dropdown

--- Tours/urls.py
+++ Tours/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
 from about.views import about_view
-
+from Home.views import PackagesView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/',include('contact.urls')),
@@ -27,5 +27,7 @@ urlpatterns = [
     path('home/',include('Home.urls')),
     path('login/',include('Signup.urls')),
     path('login/',include('Login.urls')),
-    path('about/',about_view,name='about')
+    path('about/',about_view,name='about'),
+    path('packages/',PackagesView,name='packages'),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
